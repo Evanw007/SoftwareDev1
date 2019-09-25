2334170035ffc4501d0476896d0f0c80e7981c8a
Evanw007 <55436402+Evanw007@users.noreply.github.com>
2019-09-25 13:08:35 +0100

    Add files via upload

---
+++ Evan.java
@@ -0,0 +1,21 @@
+public class Evan{
+        public static void main(String[] args){
+            String name = "Evan";
+            int age = 22;
+            String hobby = "football";
+
+            System.out.println(name +" is " + age + ". He likes playing " + hobby);
+
+            System.out.println();
+            System.out.println("Name\tHome\tHobbies");
+            System.out.println("====\t====\t=======");
+
+ System.out.println("         ***");
+ System.out.println("         ***");
+  System.out.println("        ***");
+  System.out.println("  ***   ***");
+  System.out.println("   ***  ***");
+  System.out.println("    *** **");
+   System.out.println("     ***");
+        }
+}
